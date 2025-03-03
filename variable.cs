20c8f9b4b2398c7fd917fde92737b2c844413a79
Gift-mj <jumagift1200@gmail.com>
2025-03-03 21:15:33 +0300

    Update variable.cs

--- variable.cs
+++ variable.cs
@@ -1,4 +1,4 @@
 # how to decleare variables
-##types of variables
-###how to initalise variables
-####how to retrive data of the variable
+## types of variables
+### how to initalise variables
+#### how to retrive data of the variable
